cc1d7af2bc154c12c92d7925fb5e11b66cdbf284
BerkleyDEV <88558484+BerkleyDEV@users.noreply.github.com>
2023-07-31 13:54:07 -0600

    adjusted scripts for Heroku deployment. added cors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require('cors');
+
 
 const app = express();
 require("./models/borrower.model.js");
@@ -10,6 +12,7 @@ const routes = require("./routes/index.js");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 app.use("/", routes);
 
